53cbb4af744980f0dd6fec877d5b91781c64216b
Andrew Fischer <atb.fischer@gmail.com>
2025-09-24 13:14:55 -0400

    added python scraper files

---
+++ scraper files/formula.py
@@ -0,0 +1,93 @@
+import numpy as np
+from pandas import DataFrame
+
+# This one takes a row from the html table and extracts
+# each date and its number of listeners
+def parseRow3(row):
+    timeSrch = row.find(attrs={'class':['js-date']})
+    time = str(timeSrch.attrs['datetime'])
+
+    listenersSrch = row.find(attrs={'class':['js-value']})
+    listeners = int(listenersSrch.attrs['data-value'])
+
+    return time, listeners
+
+def multiplyCells(row):
+    return float(row['playcount']) * float(row['1w'])
+
+# Uses multiplyCells above to weight each artist's listener count by their
+# playcount, then adds the resulting series to the dataframe as a new column
+def addWeight(frame):
+    result = frame.apply(multiplyCells, axis=1)
+    frame['weighted'] = result
+    return frame
+
+# Uses the logarithmic regression of the Pro users' OWLAs and Mainstream scores
+# to create an approximate Mainstream score
+def getScore(avg):
+    result = 12.4619 * np.log(avg) - 67.0731
+    return result
+
+# Uses binary search to find the artist whose OWLA is nearest the user's.
+# It can't compare the two values it lands between yet, so it only goes down for now
+def playSearch(A1, A2, n, Target):
+    L = 0
+    R = n-1
+    while (L<=R):
+        m = (L+R)//2
+        if (A1[m] < Target):
+            L = m + 1
+        elif (A1[m] > Target):
+            R = m - 1
+        else:
+            print("You're exactly as mainstream as ", A2[m-1], " (", A1[m-1], " listeners last week)", sep="")
+            return
+    print("You're about as mainstream as ", A2[m-1], " (", A1[m-1], " listeners last week)\n", sep="")
+    return
+
+def printBookends(frame):
+    print("Your most mainstream artist was ", frame.loc[len(frame)-1].iloc[1], " (", frame.loc[len(frame)-1].iloc[3], 
+      " average listeners last week)", sep="")
+    print("Your most obscure artist was ", frame.loc[0].iloc[1], " (", frame.loc[0].iloc[3], 
+      " average listeners last week)\n", sep="")
+
+def totalListenersColumn(frame):
+    lst = []
+    for i in range(len(frame)):
+        lst.append(frame['weighted'].sum())
+    frame['LstnrTotal'] = lst
+    return frame
+
+def totalPlaysColumn(frame):
+    lst = []
+    for i in range(len(frame)):
+        lst.append(frame['playcount'].sum())
+    frame['PlayTotal'] = lst
+    return frame
+
+def avgListenersColumn(frame):
+    lst = []
+    for i in range(len(frame)):
+        lst.append(frame['weighted'].sum() / frame['playcount'].sum())
+    frame['LstnrAvg'] = lst
+    return frame
+
+def georgeBailey(row):
+    # return float(row['playcount']) * float(row['1w'])
+    clarence = (row['LstnrTotal'] - row['weighted']) / (row['PlayTotal'] - row['playcount'])
+    return row['LstnrAvg'] - clarence
+
+def addGeorge(frame):
+    totalListenersColumn(frame)
+    totalPlaysColumn(frame)
+    avgListenersColumn(frame)
+
+    result = frame.apply(georgeBailey, axis=1)
+    frame['impact'] = result
+    return frame
+
+def findHeaviest(df):
+    print("Your favorite mainstream artist was ", df.loc[df['impact'].idxmax()].iloc[1], ", which you played ",
+          df.loc[df['impact'].idxmax()].iloc[2], " times", sep="")
+    print("Your favorite obscure artist was ", df.loc[df['impact'].idxmin()].iloc[1], ", which you played ",
+          df.loc[df['impact'].idxmin()].iloc[2], " times", sep="")
---
+++ scraper files/linkify.py
@@ -0,0 +1,126 @@
+# https://www.last.fm/music/Catfish+and+the+Bottlemen/The+Ride
+# https://www.last.fm/music/Tyler,+the+Creator
+# https://www.last.fm/music/Chance+the+Rapper/_/Hot+Shower
+# https://www.last.fm/music/Prince
+# Some link examples ^^
+
+# This .py file takes in an imput file of form:
+# artist1
+# playcount
+# ...
+# and creates lists of the artists, the playcounts, and links to their
+# last.fm pages
+import os
+import fnmatch
+import csv
+
+def linkifyInput(modes):
+    starter = "https://www.last.fm/music/"
+    names = []
+    playcounts = []
+
+    # take the options decided in welcome()
+    if (modes[0] == 0):
+        filetype = 'csv'
+    else:
+        filetype = 'txt'
+
+    # Same deal. This option will ignore all artists with only one play
+    if (modes[1] == 0):
+        fastMode = 'n'
+    else:
+        fastMode = 'y'
+
+    if (filetype == "txt"):
+        # Finds the number of two-line entries in the input file
+        with open(r"input.txt", 'r') as fp:
+            lines = len(fp.readlines())
+            entries = lines//2
+
+        # puts the input file's content into name and playcount lists
+        with open(r"input.txt", 'r') as fp:
+            for i in range(entries):
+                names.append(fp.readline())
+                names[i] = names[i][:-1] # remove trailing \n
+                playcounts.append(int(fp.readline()))
+
+    elif (filetype == "csv"):
+        # Maintains a count of how many plays are by each artist
+        # If an artist is in the dictionary, increment its value by 1
+        # If not, put it in the dictionary with a value of one
+        def dictCheck(artist, thisdict):
+            if (artist in thisdict):
+                thisdict[artist] += 1
+            else:
+                thisdict[artist] = 1
+        
+        # Since the ghan exporter starts its .csv files with "scrobbles",
+        # we search for that and if there's only one match we select it
+        cur_dir = os.getcwd()
+        pwdLs = os.listdir(cur_dir)
+        srch = fnmatch.filter(pwdLs, "scrobbles*")
+        if (len(srch) == 1):
+            inputCsv = srch[0]
+
+        # Reads the input .csv to a dict object, then iterates through it
+        # creating a new dictionary where each artist is a key and their
+        # playcount is the value
+        with open(inputCsv, newline='', errors='ignore') as csvfile:
+            reader = csv.DictReader(csvfile)
+            thisdict = dict()
+            for row in reader:
+                dictCheck(row['artist'], thisdict)
+
+        ## Here's where you should print out the dictionary as it stands and ask the user to set the threshold should be
+        ## but only if they have fast mode on already
+        if (fastMode == 'y'):
+            # myVals = list(thisdict.values())
+            # myVals.sort()
+
+            # sortdict = {i:thisdict[i] for i in myVals}
+            sortdict = sorted(thisdict.items(), key=lambda kv: (kv[1], kv[0]))
+            print(sortdict)
+            print("\nThese are the artists in your history with playcounts. Enter the desired threshold:")
+            thresh = int(input())
+
+        # Append the names and playcounts to their lists
+        # For fast mode it checks if the artist has enough plays to clear the threshold first
+        if (fastMode == 'y'):
+            for x, y in thisdict.items():
+                if (int(y) > thresh):
+                    names.append(x)
+                    playcounts.append(int(y))
+        else:
+            # entries = len(thisdict)
+            for x, y in thisdict.items():
+                names.append(x)
+                playcounts.append(int(y))
+
+    links = []
+
+    namesCopy = names[:]
+
+    # replaces spaces with plusses and fixes a few other trouble characters
+    for i in range(len(names)):
+        tempstr = ""
+        for char in namesCopy[i]:
+            if (char == " "):
+                tempstr += "+"
+            elif (char == "/"):
+                tempstr += "%"
+                tempstr += "2F"
+            elif (char == "?"):
+                tempstr += "%"
+                tempstr += "3F"
+            elif (char == "+"):
+                tempstr += "%"
+                tempstr += "252B"
+            else:
+                tempstr += char
+        namesCopy[i] = tempstr
+    
+    for i in range(len(namesCopy)):
+        links.append(starter + namesCopy[i])
+
+    # print(links)
+    return names, playcounts, links
